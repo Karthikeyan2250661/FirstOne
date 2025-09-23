87fefe758d30068a4820e7a0dbf7123e8317943c
Karthikeyan2250661 <164018107+Karthikeyan2250661@users.noreply.github.com>
2025-09-23 15:23:03 +0530

    Update invalid_exception.py

--- B/invalid_exception.py
+++ B/invalid_exception.py
@@ -1,1 +1,10 @@
+##DO NOT CHANGE THE GIVEN TEMPLATE. WRITE YOUR CODE IN THE PROVIDED PLACES ALONE.
+
+class InvalidRoomTypeException(Exception):
+    message = ""
+    
+    ## Create the constructor here
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
